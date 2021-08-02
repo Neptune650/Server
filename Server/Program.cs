a0fe8e8d69863a4c3f8bb80f6ce177d1ce816a84
Sapphire <villagerlover52@hotmail.com>
2021-08-02 02:57:32 -0300

    Initial commit

---
+++ Server/Program.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using IdGen;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SQLite;
+
+public class UsersContainer
+{
+    public class Users
+    {
+        [PrimaryKey]
+        public string Token { get; set; }
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public byte[] Password { get; set; }
+        public byte[] Salt { get; set; }
+        public string Username { get; set; }
+        public string Usernumber { get; set; }
+        public string Groups { get; set; }
+    }
+}
+
+public class GroupsContainer
+{
+    public class Groups
+    {
+        [PrimaryKey]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public string Chats { get; set; }
+        public string Members { get; set; }
+    }
+}
+
+public class InvitesContainer
+{
+    public class Invites
+    {
+        [PrimaryKey]
+        public string Code { get; set; }
+        public string Group { get; set; }
+    }
+}
+
+public class InvitesContainerSuccess
+{
+    public class Invites
+    {
+        public bool Success { get; set; }
+        public string Code { get; set; }
+        public string Group { get; set; }
+    }
+}
+
+public class WebsocketObjectContainer
+{
+
+    public class Message
+    {
+        public int Event { get; set; }
+        public string Id { get; set; }
+        public string Content { get; set; }
+        public nPI2UserContainer.Users Author { get; set; }
+    }
+}
+
+public class WebsocketObject2Container
+{
+
+    public class Message
+    {
+        public int Event { get; set; }
+        public string Id { get; set; }
+        public string Content { get; set; }
+    }
+}
+
+public class WebsocketObject3Container
+{
+
+    public class Message
+    {
+        public int Event { get; set; }
+        public string Id { get; set; }
+    }
+}
+
+public class WebsocketObject4Container
+{
+
+    public class Chat
+    {
+        public int Event { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
+public class WebsocketObject5Container
+{
+
+    public class Chat
+    {
+        public int Event { get; set; }
+        public string Id { get; set; }
+    }
+}
+
+public class WebsocketObject6Container
+{
+    public class Chat
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Groups
+    {
+        public int Event { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public List<Chat> Chats { get; set; }
+        public List<string> Members { get; set; }
+    }
+}
+
+public class WebsocketObject7Container
+{
+
+    public class Groups
+    {
+        public int Event { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+    }
+}
+
+public class WebsocketObject8Container
+{
+
+    public class Groups
+    {
+        public int Event { get; set; }
+        public string Id { get; set; }
+    }
+}
+
+public class GroupsContainerObjectified
+{
+    public class Message
+    {
+        public string Id { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+    }
+
+    public class Chat
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+
+    public class Groups
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public List<Chat> Chats { get; set; }
+        public List<string> Members { get; set; }
+    }
+}
+
+public class GroupsContainerSuccess
+{
+    public class MessageSuccess
+    {
+        public bool Success { get; set; }
+        public GroupsContainerObjectified.Message Message { get; set; }
+    }
+
+    public class MessagesSuccess
+    {
+        public bool Success { get; set; }
+        public List<GroupsContainerObjectified.Message> Messages { get; set; }
+    }
+
+    public class Chat
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ChatSuccess
+    {
+        public bool Success { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Groups
+    {
+        public bool Success { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public List<Chat> Chats { get; set; }
+        public List<string> Members { get; set; }
+    }
+}
+
+public class nPIUserContainer
+{
+    public class Users
+    {
+        public bool Success { get; set; }
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Usernumber { get; set; }
+        public List<string> Groups { get; set; }
+    }
+}
+
+public class nPI2UserContainer
+{
+
+    public class Users
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Usernumber { get; set; }
+    }
+
+    public class UsersSuccess
+    {
+        public bool Success { get; set; }
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Usernumber { get; set; }
+    }
+}
+
+namespace Server
+{
+    public class Program
+    {
+        public static string databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Harmony.db");
+        public static SQLiteConnection db = new SQLiteConnection(databasePath);
+        public static IdGenerator generator = new IdGenerator(0);
+
+        public static void Main(string[] args)
+        {
+            db.CreateTable<UsersContainer.Users>();
+            db.CreateTable<GroupsContainer.Groups>();
+            db.CreateTable<InvitesContainer.Invites>();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
